f838593c0d7ad67950bb78fa48a04f4ab3bec425
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-15 12:22:20 +0100

    initial commit

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <main>
+      <section>
+        <h2 className="loading">loading...</h2>
+      </section>
+    </main>
+  );
+}
+
+export default Loading;
